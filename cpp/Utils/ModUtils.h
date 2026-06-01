2b5a396ecdad16cdd98841afb0eb894dc8d909b4
mihkel365 <easyhandcoxk@atomicmail.io>
2026-05-31 23:21:01 -0400

    Update ModUtils.h

--- cpp/Utils/ModUtils.h
+++ cpp/Utils/ModUtils.h
@@ -6,26 +6,6 @@
 #include "Resolver/Il2CppResolver.h"
 #include "dobby.h"
 
-// --- THE "EASY HOOK" MACROS ---
-// These are optional. You can also use DobbyHook directly.
-
-#define DEFINE_HOOK(ret, name, ...) \
-    ret (*orig_##name)(__VA_ARGS__); \
-    ret hook_##name(__VA_ARGS__)
-
-#define INSTALL_HOOK(assembly, ns, clazz, method, args, name) \
-    { \
-        Il2CppClass* _klass = Resolver::GetClass(assembly, ns, clazz); \
-        if (_klass) { \
-            uintptr_t _target = Resolver::GetMethod(_klass, method, args); \
-            if (_target) { \
-                DobbyHook((void*)_target, (void*)hook_##name, (void**)&orig_##name); \
-                LOGI("Successfully hooked %s::%s", clazz, method); \
-            } else { LOGE("Failed to find method %s in %s", method, clazz); } \
-        } else { LOGE("Failed to find class %s", clazz); } \
-    }
-
-// --- UTILITY FUNCTIONS ---
 namespace ModUtils {
     // Example of a raw function you can call anywhere
     inline void SetPlayerSpeed(float speed) {
